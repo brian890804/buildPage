7a758bfa9e2ea077027df2e0aef3b475d1d2e378
brian890804 <71435901+brian890804@users.noreply.github.com>
2024-07-23 18:32:01 +0800

    feat:update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,243 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Carousel, Timeline } from "antd";
+import "./index.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const contentStyle = {
+    margin: 0,
+    height: "160px",
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    background: "#364d79",
+  };
+  const onChange = (currentSlide) => {
+    console.log(currentSlide);
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Timeline
+        items={[
+          {
+            color: "green",
+            children: (
+              <p>
+                <p
+                  style={{
+                    fontSize: "20px",
+                    color: "red",
+                    fontWeight: "bolder",
+                  }}
+                >
+                  我與哥吉拉社團Mr.Kaiju社員[楊世聖先生]購買疑點?!
+                </p>
+                <h5>其中有些都是用電話 所以細節不清楚 會在努力補上</h5>
+                <h5>最後更新時間2024/07/23 15:44</h5>
+              </p>
+            ),
+          },
+          {
+            color: "green",
+            children: (
+              <p style={{ color: "red" }}>
+                <p>
+                  7/7詢問[-1哥](哥吉拉公仔)並獲得楊世聖同意寄送店到店貨到付款($3600)
+                  要等他回家後寄出
+                </p>
+                <p style={{ color: "red" }}>
+                  訂完一哥後我詢問另一隻感興趣公仔[斯特洛伊亞](哥吉拉公仔)
+                  ，同時楊世聖說他有訂4隻可以給我一隻，我就和楊世聖訂購戴[斯特洛伊亞](哥吉拉公仔)金額4000含運費
+                </p>
+              </p>
+            ),
+          },
 
-export default App
+          {
+            color: "green",
+            children: (
+              <>
+                <p>
+                  7/8楊世聖告知[-1哥](哥吉拉公仔)在別的社團先被下標買走，且他的老婆已寄出
+                </p>
+                <p>
+                  <p style={{ color: "red" }}>
+                    然後楊世聖說:"下周還有一組會到 我直接出給你"
+                  </p>
+                  <p>並降價3600含運</p>
+                </p>
+              </>
+            ),
+          },
+          {
+            color: "red",
+            children: (
+              <>
+                <p>
+                  7/9
+                  楊世聖通知[-1哥](哥吉拉公仔)被棄單他太太超不爽，可以給我但是要先收訂金而尾款
+                  則是物流到達時會收(但後來電話說因上個棄單的賣家或是物流把包裹弄壞了(弄濕)不能給我需要自行處理。)
+                  而在7/8楊世聖所說的他教的另一隻公仔也不知所云
+                </p>
+                <p>
+                  告知訂購[斯特洛伊亞](哥吉拉公仔)因前面一隻公仔[-1哥](哥吉拉公仔)一直反起反落，而[斯特洛伊亞](哥吉拉公仔)便宜給我由4000降為3600
+                </p>
+                <p>同日我轉帳3660給他</p>
+              </>
+            ),
+          },
+          {
+            color: "purple",
+            children: (
+              <>
+                <p>7/9 楊世聖通知[斯特洛伊亞](哥吉拉公仔)在海關清關於7/9會到</p>
+                <p>楊世聖通知[-1哥](哥吉拉公仔)於7/10會退回她家那邊的小7</p>
+                <p>同日與楊世聖約面交周六(7/12)在汐止火車站面交時間下午1:00</p>
+              </>
+            ),
+          },
+          {
+            color: "purple",
+            children: (
+              <>
+                <p>7/12 我通知楊世聖隔日7/13可以面交嗎</p>
+                <p>楊世聖通知還沒到</p>
+              </>
+            ),
+          },
+          {
+            color: "green",
+            children: (
+              <>
+                <p>7/15 我主動詢問楊世聖清關(海關)完成了嗎</p>
+                <p>楊世聖回復待轉運了 快的話週三可到(7/17)</p>
+              </>
+            ),
+          },
+          {
+            color: "green",
+            children: (
+              <>
+                <p>7/17 我主動詢問今天有到嗎~</p>
+                <p>楊世聖回轉運了 快的話週五可到(7/19)</p>
+              </>
+            ),
+          },
+          {
+            color: "green",
+            children: (
+              <>
+                <p>7/19 我主動詢問商品到了嗎到了今天可以拿</p>
+                <p>
+                  楊世聖回電說新竹物流運送中等待配送，假日可能沒營業要等下周，
+                </p>
+                <p> 我跟他要物流單號她說會請海關提供 (沒給)</p>
+              </>
+            ),
+          },
+          {
+            color: "green",
+            children: (
+              <>
+                <p>7/22 我主動詢問物流單號</p>
+                <p>
+                  楊世聖回電說新竹物流不收進口貨物因為超過規範，他會親自去拿並於周三(週三有大機率颱風假)(7/24)與我面交
+                </p>
+                <p>我跟他要物流單號她說會請海關提供</p>
+              </>
+            ),
+          },
+          {
+            color: "green",
+            children: (
+              <>
+                <p>7/23 因等待時間過長我主動詢問EZWAY進口畫面</p>
+                <p>
+                  楊世聖傳了一張給我但是這張圖片日期的部分是被圈起來的
+                  202X/06/28 (X的部分被圈起來 但是有沒圈好的地方 明明顯示是2)
+                </p>
+                <p>
+                  後來我與他再次通話他不是很開心地回答，我收回EZWAY畫面是因為個資問題
+                  那你帳號給我我這邊直接退款給你
+                </p>
+                <p>後來協議保持原樣面交</p>
+              </>
+            ),
+          },
+          {
+            color: "red",
+            children: (
+              <p>
+                ----------------------------------------------------------------------
+              </p>
+            ),
+          },
+          {
+            color: "red",
+            children: (
+              <p>
+                <p>疑點</p>
+                <p>
+                  1.楊世聖告知因體積問題新竹物流拒收，而後我問他運了甚麼，他說只運我的公仔
+                  (大小大概40*30)怎麼可能新竹物流會不收
+                </p>
+                <p>
+                  2.楊世聖傳給我的EZWAY清關畫面雖日期被圈塗起來
+                  但是還是隱約看到的年份是(2022而非2024)
+                </p>
+                <p>
+                  3.在網路上可以搜尋到多筆關於楊世聖的刑事紀錄、支付命令，而有多筆小額詐欺金額2000~6000不等
+                  懷疑挖東牆補西牆
+                </p>
+                <p>
+                  4.我發現EZWAY年份不符合(雖有稍微被遮擋但隱約看的出來)跟楊世聖說(他不是很高興的跟我說要退款)
+                  這邊我就更篤定我的猜想
+                  他是借東補西假借買賣名義實則填補小額財務漏洞，
+                  同時詐騙多位受害人，並沒有出貨的打算。
+                </p>
+                <p>5.楊世聖說7/9清關完成 但是EZWAY上面寫202X/06/28</p>
+                <p>
+                  6.楊世聖於7/9說他有訂購4隻[斯特洛伊亞](哥吉拉公仔)
+                  [斯特洛伊亞](哥吉拉公仔) 但於7/23跟我說只運送我的那隻而已?
+                </p>
+              </p>
+            ),
+          },
+          {
+            color: "red",
+            children: (
+              <p>
+                ----------------------------------------------------------------------
+              </p>
+            ),
+          },
+          {
+            color: "red",
+            children: (
+              <p>
+                <p>結</p>
+                <p>
+                  感謝大家看，購買任何東西請確認對方個人資料是否充足，最好以面交為主，若有任何疑慮先去備案，不要幫對方想好理由合理化依竊。
+                </p>
+              </p>
+            ),
+          },
+          {
+            color: "red",
+            children: (
+              <p>
+                <p>佐證圖片</p>
+                <p></p>
+              </p>
+            ),
+          },
+        ]}
+      />
+      <Carousel afterChange={onChange}>
+        <div>{/* <img src={pic1} alt="pic1" /> */}</div>
+        <div>{/* <img src={pic1} alt="pic2" /> */}</div>
+        <div>{/* <img src={pic1} alt="pic3" /> */}</div>
+        <div>{/* <img src={pic1} alt="pic4" /> */}</div>
+      </Carousel>
+    </>
+  );
+};
+export default App;
